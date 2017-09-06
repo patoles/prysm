961c46c980b4b2aa7476fd55d9b8f57f08b9359e
Simon P <simon.patole@curiositychina.com>
2017-09-06 14:08:36 +0800

    Change libs path

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,15 @@ new Prysm(target, {
     fragment:"water",
     vertex:"water",
     params:{
-        fragment:{speed:0.02, amplitude:10.1, refraction:0.8, width:0.12},
-        vertex:{amplitude:0.05, frequency:1}
+        fragment:{
+            speed:0.02,
+            amplitude:10.1,
+            refraction:0.8,
+            width:0.12
+        },
+        vertex:{
+            amplitude:0.12,
+            frequency:1
+        }
     }
 });
